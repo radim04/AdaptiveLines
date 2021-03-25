346971f57541e2a9fa8e785e7e6fb11169d60e5e
Radim Sykora <radim.sykora@sap.com>
2021-03-25 20:17:39 +0100

    dali displayed

--- AdaptiveLines.js
+++ AdaptiveLines.js
@@ -1,23 +1,31 @@
 var lines = [];
+var img;
+
+function preload() {
+  img = loadImage('data/dali.jpg');
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(255);
-  initialize();
+  background(155);
+  createLines();
 }
 
 function draw() {
+  image(img, 0, 0);
   for(var i=0; i<lines.length; i++) {
     lines[i].display();
   }
 }
 
-function initialize() {
+function createLines() {
   for(var i=0; i<50; i++) {
     l = new Line(5, 5 + 10*i, width-5, 5 + 10*i);
     lines.push(l);
   }
 }
 
-function mousePressed() {
+function createImage() {
+  img = createImage(200, 200);
+  img.loadPixels();
 }
